3bd576963786aede866fd5345c925a0f48790e1d
Abinet <abinetmulugeta5@gmail.com>
2026-08-13 14:46:44 +0300

    Connect Relstate to PostgreSQL

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 import app from './app.js';
-
+import pool from "./database/db.js";
 const PORT = process.env.PORT || 4000;
 
 const server = app.listen(PORT, () => {
@@ -14,4 +14,23 @@ process.on('SIGINT', () => {
 process.on('SIGTERM', () => {
   console.log('SIGTERM received. Closing server...');
   server.close(() => process.exit(0));
+});
+
+app.get("/api/health", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+
+    res.json({
+      success: true,
+      postgres: "connected",
+      time: result.rows[0].now
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      success: false,
+      postgres: "connection failed"
+    });
+  }
 });
