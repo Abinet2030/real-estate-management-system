0deeff4a71b00db05e489b014d66ebb01cc54905
Abinet <abinetmulugeta5@gmail.com>
2026-08-15 11:49:06 +0300

    fix: finalize Vercel server layout and API entrypoints

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
+
 import propertiesRouter from './routes/properties.js';
 import authRouter from './routes/auth.js';
 import uploadsRouter from './routes/uploads.js';
