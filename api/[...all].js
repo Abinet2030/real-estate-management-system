5e84940234e65503ac6dab150df9ce2a936604e8
Abinet <abinetmulugeta5@gmail.com>
2026-08-15 11:28:41 +0300

    fix: remove Vercel rewrite and direct /api handling

--- api/[...all].js
+++ api/[...all].js
@@ -1,8 +1,5 @@
 import app from '../app.js';
 
 export default function handler(req, res) {
-  if (typeof req.url === 'string' && req.url.startsWith('/api')) {
-    req.url = req.url.replace(/^\/api/, '') || '/';
-  }
   return app(req, res);
 }
